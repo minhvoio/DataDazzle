c670f233ff3fec6e694abbbaedefea83dddb7cba
minhvo0o <vdminh@penphy.edu.vn>
2024-03-31 15:50:14 +1100

    Added client directory

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import DragComponent from "./DragDrop/DragComponent";
+
+function App() {
+  return (
+    <div className="flex h-screen justify-center items-center px-5 bg-slate-100">
+      <DragComponent />
+    </div>
+  );
+}
+
+export default App;
